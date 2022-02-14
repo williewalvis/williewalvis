bbc472825b263abb06ccecc9ebe82dc1fb10976e
Wilmar Malherbe <wilmar.malherbe@espdevrbx.com>
2022-02-14 21:07:00 +0200

    some stuffs

--- web/js/home.js
+++ web/js/home.js
@@ -1,23 +1,42 @@
+let currentSong_Name = ""
+
 setInterval(() => {
     $.ajax({
         url: "/spotify/getPlayState",
         type: "GET",
         success: async function (result) {
             if (result.data.is_playing) {
-                // take away name
-                await $('#animated-name').animate({ opacity: 0 }, 300)
-                // change visibility
-                $('#card-main').animate({ opacity: 1 }, 300)
                 // reference data as variable
                 let item = result.data.item
-                // change image
-                $("#image_cover").attr("src", `${item.album.images[0].url}`)
-                // change song name and artist name
-                $("#song_name").html(`<strong>${item.name}</strong> • ${item.artists[0].name}`)
-                // change spotify button uri
-                $("#spotify_uri").attr("href", `${item.uri}`)
+                // take away name
+                await $('#animated-name').animate({ opacity: 0 }, 100)
+                if (currentSong_Name !== item.name) {
+                    // new song started
+                    $('#card-main').animate({ opacity: 0 }, 100)
+                    // change image
+                    $("#image_cover").attr("src", `${item.album.images[0].url}`)
+                    // change song name and artist name
+                    $("#song_name").html(`<strong>${item.name}</strong> • ${item.artists[0].name}`)
+                    // change spotify button uri
+                    $("#spotify_uri").attr("href", `${item.uri}`)
+                    // do some stuffs
+                    $('#card-main').animate({ opacity: 1 }, 100)
+                    // set name variable
+                    currentSong_Name = item.name
+                } else {
+                    // change visibility
+                    $('#card-main').animate({ opacity: 1 }, 300)
+                    // change image
+                    $("#image_cover").attr("src", `${item.album.images[0].url}`)
+                    // change song name and artist name
+                    $("#song_name").html(`<strong>${item.name}</strong> • ${item.artists[0].name}`)
+                    // change spotify button uri
+                    $("#spotify_uri").attr("href", `${item.uri}`)
+                    // set name variable
+                    currentSong_Name = item.name
+                }
             } else {
-                await $('#card-main').animate({ opacity: 0 }, 300)
+                await $('#card-main').animate({ opacity: 0 }, 100)
                 $('#animated-name').animate({ opacity: 1 }, 300)
             }
         },
