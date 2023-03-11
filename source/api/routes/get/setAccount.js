80b2d7c9a69927dab264ac1734f438445d789da1
Wilmar Malherbe <wilmar.malherbe@williewalvis.co.za>
2023-03-11 10:43:39 +0200

    transfer route into seperate file: setAccount

---
+++ source/api/routes/get/setAccount.js
@@ -0,0 +1,69 @@
+// endpoint
+const Express = require("express")
+const SpotifyHandler = require("../../spotifyHandler")
+
+let base = null
+const route = "/spotify/setAccount"
+
+module.exports = {
+    Name: "Set Account",
+    Description: "This is an API endpoint, designed to run a specific function.",
+    AccessPoint: `${route}`,
+    Initialize: async (Input) => {
+        base = Input
+    },
+    /**
+     * Internal JSDoc for the sanity of my brain.
+     * @param {Express.Request} req The request object from Express.
+     * @param {Express.Response} res The response object from Express.
+     * @returns {void} Returns nothing to internal server, only sent to client.
+     */
+    Function: async (req, res) => {
+
+        // initiate try catch
+        try {
+
+            // check if user is in query
+            if (req.query.user && req.query.user == process.env.USER) {
+
+                // run function to set auth token
+                SpotifyHandler.createAuthorizeURL()
+
+                    // function when promise is resolved
+                    .then(async (redir) => {
+
+                        // send response to client
+                        res.redirect(redir)
+
+                    })
+
+                    // function when promise is rejected
+                    .catch(async (err) => {
+
+                        // send response to client
+                        res.send("There was a problem while creating the link.")
+
+                    })
+
+            } else {
+
+                // send response to client
+                res.send("No identification was provided for this request.")
+
+            }
+
+        } catch (err) {
+
+            // log error and send response
+            console.log("Error while running, x@214, " + new Date(Date.now()).toString())
+
+            // send to response
+            res.send("The server could not respond.")
+
+        }
+
+    },
+
+}
+
+// end 
